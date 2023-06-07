8399eefb20771934e9013540463a8d3c9dd13854
dragonccm <121989450+dragonccm@users.noreply.github.com>
2023-06-07 22:34:56 +0700

    Co-authored-by: rurimeiko@outlook.com <rurimeiko@outlook.com> Co-authored-by: Hung-Thinhh <Hung-Thinhh@users.noreply.github.com> Co-authored-by: dawn1810 <dawn1810@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,39 +1,44 @@
 const header_menu = document.querySelector('.header_menu')
 const left_item = document.querySelector('.left_item')
 
-header_menu.onclick = function(event){
-    event.stopPropagation();
-    left_item.classList.remove('close')
+let menuBtnCount = true;
+
+header_menu.onclick = function (event) {
+    if (menuBtnCount) {
+        event.stopPropagation();
+        left_item.classList.remove('close');
+        menuBtnCount = false;
+    }
+    else {
+        left_item.classList.add('close');
+        menuBtnCount = true;
+    }
+
 }
-document.querySelector('body').onclick = function(){
+
+document.querySelector('body').onclick = function () {
     left_item.classList.add('close')
 }
 
-
-var container = document.querySelector('.trending_novel');
-    var scrollAmount = 200; 
-
-    function scrollLeft() {
-        container.scrollBy({
-            left: -scrollAmount,
-            behavior: 'smooth'
-        });
-    }
-
-    function scrollRight() {
-        container.scrollBy({
-            left: scrollAmount,
-            behavior: 'smooth'
-        });
-    }
-
-    var scrollLeftBtn = document.createElement('button');
-    scrollLeftBtn.innerHTML = '<';
-    scrollLeftBtn.addEventListener('click', scrollLeft);
-
-    var scrollRightBtn = document.createElement('button');
-    scrollRightBtn.innerHTML = '>';
-    scrollRightBtn.addEventListener('click', scrollRight);
-
-    document.body.insertBefore(scrollLeftBtn, container);
-    document.body.appendChild(scrollRightBtn);
+//
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
