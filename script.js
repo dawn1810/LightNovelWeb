0c546d2c7231196adfa0eeddda3f99c1a870f58a
dragonccm <121989450+dragonccm@users.noreply.github.com>
2023-06-07 23:54:58 +0700

    modified:   Index.html 	modified:   script.js

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const header_menu = document.querySelector('.header_menu')
 const left_item = document.querySelector('.left_item')
+const category_btn = document.getElementById('category')
+const category_list = document.querySelector('.category_list')
 
 let menuBtnCount = true;
+
+category_btn.onclick = function () {
+    category_list.style.display = 'block';
+}
+document.onclick = function (event) {
+    const targetElement = event.target;
+    if (!category_list.contains(targetElement) && targetElement !== category_btn) {
+        category_list.style.display = 'none';
+    }
+}
 
 header_menu.onclick = function (event) {
     if (menuBtnCount) {
@@ -15,30 +27,36 @@ header_menu.onclick = function (event) {
     }
 
 }
+const category = document.querySelector('.category')
 
-document.querySelector('body').onclick = function () {
-    left_item.classList.add('close')
+left_item.onclick = function (event) {
+    event.stopPropagation();
+    console.log('ok')
 }
+
+// document.querySelector('body').onclick = function () {
+//     left_item.classList.add('close')
+// }
 
 //
 const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
-  
+
     // If we need pagination
     pagination: {
-      el: '.swiper-pagination',
+        el: '.swiper-pagination',
     },
-  
+
     // Navigation arrows
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     },
-  
+
     // And if we need scrollbar
     scrollbar: {
-      el: '.swiper-scrollbar',
+        el: '.swiper-scrollbar',
     },
-  });
+});
