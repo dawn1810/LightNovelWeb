c060853efd8808832405f0fb2cc77640435020ca
dragonccm <121989450+dragonccm@users.noreply.github.com>
2023-06-09 15:49:39 +0700

    Co-authored-by: dawn1810 <dawn1810@users.noreply.github.com> Co-authored-by: Hung-Thinhh <Hung-Thinhh@users.noreply.github.com> Co-authored-by: rurimeiko@outlook.com <rurimeiko@outlook.com>

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const header_menu = document.querySelector('.header_menu')
 const left_item = document.querySelector('.left_item')
 const category_btn = document.getElementById('category')
 const category_list = document.querySelector('.category_list')
+const summary_Content= document.querySelector('.summary-Content')
+const summary_btn = document.querySelector('.summary-btn')
 
 let menuBtnCount = true;
 
@@ -34,6 +36,15 @@ left_item.onclick = function (event) {
     console.log('ok')
 }
 
+function toggleSummary() { 
+    if (summary_Content.innerHTML === '') {
+      summary_Content.innerHTML = 'Murasaki có khả năng nhìn thấy tương lai của người mà cô ấy chạm vào. Là con gái của chủ nhà, hàng tháng cô phải đuổi theo Shijima Banri để lấy tiền thuê nhà. Lần nào cũng vậy, tác giả tội nghiệp Shijima hứa với cô rằng anh ta sẽ trả tiền thuê nhà ngay khi được xuất bản và được trả tiền. Murasaki giữ bí mật về khả năng nhìn thấy tương lai của mình và rất mong chờ nó, bởi vì cô thấy Shijima là nửa kia định mệnh mà cô sẽ kết hôn.';
+      summary_btn.innerHTML = '<i class="fa-solid fa-angles-up"></i> Ẩn tóm tắt';
+    } else {
+      summary_Content.innerHTML = '';
+      summary_btn.innerHTML = '<i class="fa-solid fa-bars"></i> TÓM TẮT TRUYỆN';
+    }
+  }
 // document.querySelector('body').onclick = function () {
 //     left_item.classList.add('close')
 // }
