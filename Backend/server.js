aa0d46fe64f80391dfe73e8527addb2db99d702e
dawn1810 <106864922+dawn1810@users.noreply.github.com>
2023-06-26 20:13:57 +0700

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -715,14 +715,16 @@ app.get(
 );
 
 app.get('/auth/facebook',
-	passport.authenticate('facebook'));
+	passport.authenticate('facebook')
+);
 
 app.get('/auth/facebook/callback',
 	passport.authenticate('facebook', { failureRedirect: '/login' }),
 	function (req, res) {
 		// Successful authentication, redirect home.
 		res.send('<script>window.close();</script>');
-	});
+	}
+);
 
 // Reviews page ---------------------------------------------------------------------------------------------------------------------------------------------------
 app.get('/reviews/:id', async (req, res) => {
