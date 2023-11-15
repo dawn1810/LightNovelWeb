c6dc5d08dfada6f822d26ad5d47c752838b286f3
dawn1810 <binhminh19112003@gmail.com>
2023-11-15 12:18:56 +0700

    modified:   backend/controller/profile.controller.js

--- backend/controller/profile.controller.js
+++ backend/controller/profile.controller.js
@@ -54,60 +54,83 @@ const processNovels = async function (req, res, id_truyen) {
     let result_like = [];
 
     for (let novel of myNovels) {
-      const id = novel.id;
       // const curr_novel = await server.find_one_Data("truyen", {
       //   _id: new ObjectId(id),
       // });
-      // const genres = await queryAsync(`
-      // SELECT 
-      //   the_loai.ten_the_loai
-      // FROM the_loai
-      // INNER JOIN tacgia
-      //   ON truyen.id_tac_gia = tacgia.id
-      // INNER JOIN thongtin_nguoidung
-      //   ON tacgia.id_nguoi_dung = thongtin_nguoidung.id
-      // WHERE thongtin_nguoidung.id = '${account.id}'
-      // `);
+      novel.genres = await queryAsync(`
+      SELECT 
+        the_loai.ten_the_loai
+      FROM the_loai
+      INNER JOIN the_loai_truyen
+        ON the_loai_truyen.id_the_loai = the_loai.id
+      INNER JOIN truyen
+        ON the_loai_truyen.id_truyen = truyen.id
+      WHERE truyen.id = '${novel.id}'
+      `).map((genre) => genre.ten_the_loai);
+
+      const chapters = await queryAsync(`
+      SELECT 
+        chuong.ten_chuong,
+        chuong.noi_dung_chuong
+      FROM chuong
+      INNER JOIN truyen
+        ON chuong.id_truyen = truyen.id
+      WHERE truyen.id = '${novel.id}'
+      `);
+
+      novel.chap_ids = chapters.map((chapter) => chapter.noi_dung_chuong);
+      novel.name_chaps = chapters.map((chapter) => chapter.ten_chuong);
 
       result.push(curr_novel);
-      if (id == id_truyen) {
+      if (novel.id == id_truyen) {
         render_data.edit_name = novel.ten_truyen;
         render_data.edit_auth = novel.ten_tac_gia;
         render_data.edit_status = novel.trang_thai;
-        render_data.edit_tag = curr_novel.genres;
+        render_data.edit_tag = novel.genres;
         render_data.edit_review = novel.tom_tat_noi_dung;
         render_data.edit_img = novel.anh_dai_dien;
-        render_data.edit_chap_ids = curr_novel.chap_ids;
-        render_data.edit_name_chaps = curr_novel.name_chaps;
+        render_data.edit_chap_ids = novel.chap_ids;
+        render_data.edit_name_chaps = novel.name_chaps;
       }
     }
-
+    
     render_data.novels = result;
+    const likeNovels = await queryAsync (`
+      SELECT 
+        truyen.id, 
+        truyen.ten_truyen, 
+        truyen.trang_thai,
+        truyen.tom_tat_noi_dung,
+        truyen.anh_dai_dien
+      FROM truyen
+      INNER JOIN truyen_yeu_thich
+        ON truyen.id = truyen_yeu_thich.id_truyen
+      INNER JOIN thongtin_nguoidung
+        ON truyen_yeu_thich.id_nguoi_dung = thongtin_nguoidung.id
+      WHERE thongtin_nguoidung.id = '${account.id}'
+    `);
     let idListlikeNovels = [];
-    for (let id of novels.likeNovels) {
-      let curr_novel = await server.find_one_Data("truyen", {
-        _id: new ObjectId(id),
-      });
-      if (!curr_novel) {
-        await server.update_one_Data(
-          "tt_nguoi_dung",
-          { _id: decodeList[1] },
-          {
-            $pull: { likeNovels: id },
-          }
-        );
-      } else {
-        idListlikeNovels.push(id);
+    for (let novel of likeNovels) {
+      // if (!novel) {
+      //   await server.update_one_Data(
+      //     "tt_nguoi_dung",
+      //     { _id: account.id },
+      //     {
+      //       $pull: { likeNovels: id },
+      //     }
+      //   );
+      // } else {
+        idListlikeNovels.push(novel.id);
 
-        curr_novel.update_date = func_controller.calTime(curr_novel.update_date);
-        result_like.push(curr_novel);
-      }
+        novel.ngay_cap_nhat = func_controller.calTime(novel.ngay_cap_nhat);
+        result_like.push(novel);
+      // }
     }
     render_data.like_novel = idListlikeNovels;
     render_data.like_novel_list = result_like;
 
     if (id_truyen) {
-      if (!novels.mynovel.includes(id_truyen)) {
+      if (myNovels.some(novel => novel.id !== id_truyen)) {
         res.status(403).send("Lỗi, không có quyền truy cập!");
       }
     }
