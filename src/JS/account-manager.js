b926860be24dc5161fefb1caa8e3b4efab6bdf4d
Hung-Thinhh <hungthinhh2003@gmail.com>
2023-11-25 17:08:44 +0700

    modified:   src/JS/account-manager.js

--- src/JS/account-manager.js
+++ src/JS/account-manager.js
@@ -4,192 +4,145 @@ document.addEventListener("DOMContentLoaded", function () {
   let authorf5 = document.querySelectorAll(".author_btn_f5");
   let author = document.querySelectorAll(".author_btn");
 
-  const account_block_func = () => {
-    accountElements = document.querySelectorAll(".block_account");
-    for (const block_account of accountElements) {
-      block_account.addEventListener("click", function (e) {
-        e.preventDefault();
-        console.log(
-          block_account.parentElement.parentElement.getAttribute("id")
-        );
-        func_block_account(
-          block_account,
-          block_account.parentElement.parentElement.getAttribute("id")
-        );
-      });
-    }
-  };
-
-  const account_open_func = () => {
-    accountf5 = document.querySelectorAll(".block_account_f5 ");
-    console.log(accountf5);
-    for (const open_account of accountf5) {
-      open_account.addEventListener("click", function (e) {
-        e.preventDefault();
-        console.log(
-          open_account.parentElement.parentElement.getAttribute("id")
-        );
-        func_open_account(
-          open_account,
-          open_account.parentElement.parentElement.getAttribute("id")
-        );
-      });
-    }
-  };
+ 
 
   const author_open_func = () => {
-    author = document.querySelectorAll(".author_btn");
-    for (const open_account of author) {
-      open_account.addEventListener("click", function (e) {
-        e.preventDefault();
-        console.log(
-          open_account.parentElement.parentElement.getAttribute("id")
-        );
-        func_block_author(
-          open_account,
-          open_account.parentElement.parentElement.getAttribute("id")
-        );
-      });
+    authorf5 = document.querySelectorAll(".author_btn_f5");
+    if (authorf5) {
+      for (const open_account of authorf5) {
+        open_account.addEventListener("click", function (e) {
+          e.preventDefault();
+          console.log(
+            open_account.parentElement.parentElement.getAttribute("id")
+          );
+          func_open_author(
+            open_account,
+            open_account.parentElement.parentElement.getAttribute("id")
+          );
+        });
+      }
     }
   };
 
   const author_block_func = () => {
     author = document.querySelectorAll(".author_btn");
-    for (const open_account of author) {
-      open_account.addEventListener("click", function (e) {
-        e.preventDefault();
-        console.log(
-          open_account.parentElement.parentElement.getAttribute("id")
-        );
-        func_block_author(
-          open_account,
-          open_account.parentElement.parentElement.getAttribute("id")
-        );
-      });
+    if (author) {
+      for (const open_account of author) {
+        open_account.addEventListener("click", function (e) {
+          e.preventDefault();
+          console.log(
+            open_account.parentElement.parentElement.getAttribute("id")
+          );
+          func_block_author(
+            open_account,
+            open_account.parentElement.parentElement.getAttribute("id")
+          );
+        });
+      }
     }
   };
 
-  console.log(accountElements);
+  const account_block_func = () => {
+  const accountElements = document.querySelectorAll(".block_account");
   if (accountElements) {
     for (const block_account of accountElements) {
       block_account.addEventListener("click", function (e) {
         e.preventDefault();
-        console.log(
-          block_account.parentElement.parentElement.getAttribute("id")
-        );
-        func_block_account(
-          block_account,
-          block_account.parentElement.parentElement.getAttribute("id")
-        );
+        console.log(block_account.parentElement.parentElement.getAttribute("id"));
+        func_block_account(block_account, block_account.parentElement.parentElement.getAttribute("id"));
       });
     }
   }
-  const func_block_account = async (element, id) => {
-    const url = `/api/block_account`;
+};
 
-    const requestOptions = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        id: id,
-      }),
-    };
-
-    try {
-      const response = await fetch(url, requestOptions);
-
-      if (response.status === 200) {
-        const data = await response.json();
-        notify("n", "Thay đổi thông tin thành công!");
-        element.classList.remove("block_account");
-        element.classList.add("block_account_f5");
-        element.innerHTML = "Mở khoá người dùng";
-        element.parentElement.children[1].classList.add("last");
-        element.parentElement.children[1].style.pointerEvents = 'none';
-        author_block_func();
-        author_open_func();
-        account_block_func();
-        account_open_func();
-      } else {
-        notify("x", response.statusText);
-      }
-    } catch (error) {
-      console.log("Error:", error);
-    }
-  };
-
-  console.log(accountf5);
+const account_open_func = () => {
+  const accountf5 = document.querySelectorAll(".block_account_f5");
   if (accountf5) {
     for (const open_account of accountf5) {
       open_account.addEventListener("click", function (e) {
         e.preventDefault();
-        console.log(
-          open_account.parentElement.parentElement.getAttribute("id")
-        );
-        func_open_account(
-          open_account,
-          open_account.parentElement.parentElement.getAttribute("id")
-        );
+        console.log(open_account.parentElement.parentElement.getAttribute("id"));
+        func_open_account(open_account, open_account.parentElement.parentElement.getAttribute("id"));
       });
     }
   }
-  const func_open_account = async (element, id) => {
-    const url = `/api/open_account`;
+};
 
-    const requestOptions = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        id: id,
-      }),
-    };
+const func_block_account = async (element, id) => {
+  const url = `/api/block_account`;
 
-    try {
-      const response = await fetch(url, requestOptions);
-
-      if (response.status === 200) {
-        const data = await response.json();
-        console.log(data.role[0].role);
-        notify("n", "Thay đổi thông tin thành công!");
-        if (data.role[0].role == "1") {
-          element.classList.remove("block_account_f5");
-          element.classList.add("block_account");
-          element.innerHTML = "Khoá người dùng";
-          element.parentElement.children[1].classList.remove("last");
-          element.parentElement.children[1].style.pointerEvents = 'auto';
-          author_block_func();
-          author_open_func();
-          account_block_func();
-          account_open_func();
-        } else {
-          console.log(data.role[0].role);
-
-          element.parentElement.innerHTML = `
-        <div class="block_account ">
-        Khoá người dùng
-      </div>
-      <div class="author_btn open ">
-        Xoá quyền tác giả
-      </div>
-        `;
-          author_block_func();
-          author_open_func();
-          account_block_func();
-          account_open_func();
-        }
-      } else {
-        notify("x", response.statusText);
-      }
-    } catch (error) {
-      console.log("Error:", error);
-    }
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      id: id,
+    }),
   };
 
-  console.log(author);
+  try {
+    const response = await fetch(url, requestOptions);
+
+    if (response.status === 200) {
+      const data = await response.json();
+      notify("n", "Thay đổi thông tin thành công!");
+      element.classList.remove("block_account");
+      element.classList.add("block_account_f5");
+      element.innerHTML = "Mở khoá người dùng";
+      element.parentElement.children[1].classList.add("last");
+      element.parentElement.children[1].style.pointerEvents = "none";
+    } else {
+      notify("x", response.statusText);
+    }
+  } catch (error) {
+    console.log("Error:", error);
+  }
+
+  account_block_func();
+  account_open_func();
+};
+
+const func_open_account = async (element, id) => {
+  const url = `/api/open_account`;
+
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      id: id,
+    }),
+  };
+
+  try {
+    const response = await fetch(url, requestOptions);
+
+    if (response.status === 200) {
+      const data = await response.json();
+      console.log(data.role[0].role);
+      notify("n", "Thay đổi thông tin thành công!");
+
+      element.classList.remove("block_account_f5");
+      element.classList.add("block_account");
+      element.innerHTML = "Khoá người dùng";
+      element.parentElement.children[1].classList.remove("last");
+      element.parentElement.children[1].style.pointerEvents = "auto";
+    } else {
+      notify("x", response.statusText);
+    }
+  } catch (error) {
+    console.log("Error:", error);
+  }
+
+  account_block_func();
+  account_open_func();
+};
+
+account_block_func();
+account_open_func();
+
   if (author) {
     for (const open_account of author) {
       open_account.addEventListener("click", function (e) {
@@ -224,19 +177,14 @@ document.addEventListener("DOMContentLoaded", function () {
         const data = await response.json();
         notify("n", "Thay đổi thông tin thành công!");
 
-        element.parentElement.innerHTML = `
-        <div class="block_account ">
-        Khoá người dùng
-      </div>
-      <div class="author_btn_f5 open">
-      Thêm quyền tác giả
-    </div>
-        `;
+        element.classList.remove("author_btn");
+        element.classList.add("author_btn_f5");
+        element.innerHTML = "Thêm quyền tác giả";
 
-        author_block_func();
+        // author_block_func();
         author_open_func();
-        account_block_func();
-        account_open_func();
+        // account_block_func();
+        // account_open_func();
       } else {
         notify("x", response.statusText);
       }
@@ -245,7 +193,6 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   };
 
-  console.log(authorf5);
   if (authorf5) {
     for (const open_account of authorf5) {
       open_account.addEventListener("click", function (e) {
@@ -280,19 +227,14 @@ document.addEventListener("DOMContentLoaded", function () {
         const data = await response.json();
         notify("n", "Thay đổi thông tin thành công!");
 
-        element.parentElement.innerHTML = `
-        <div class="block_account ">
-        Khoá người dùng
-      </div>
-      <div class="author_btn open">
-      Xoá quyền tác giả
-    </div>
-        `;
+        element.classList.remove("author_btn_f5");
+        element.classList.add("author_btn");
+        element.innerHTML = "Xoá quyền tác giả";
 
+        // author_open_func();
         author_block_func();
-        author_open_func();
-        account_block_func();
-        account_open_func();
+        // account_block_func();
+        // account_open_func();
       } else {
         notify("x", response.statusText);
       }
